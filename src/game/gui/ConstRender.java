ccd7915d9a91a31fff4018666be6af7d096561b1
Aedan Smith <aedan.smith@gmail.com>
2016-08-04 13:05:28 -0700

    Fixed GUI and Inventory code to work with new Positions.

---
+++ src/game/gui/ConstRender.java
@@ -0,0 +1,12 @@
+package game.gui;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Aedan Smith on 7/30/2016.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConstRender {
+}
